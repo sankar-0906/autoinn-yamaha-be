2df189e193fc55fab1217de9bb8a8196ac4d4218
sankar <sankar.r@codingmart.com>
2026-04-25 14:44:25 +0530

    deployed issue fixed

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ import logger from './utils/logger.js';
 
 dotenv.config();
 
-import './modules/vehicleStockInward/vehicleStockInward.worker.js';
+if (!process.env.VERCEL) {
+    import('./modules/vehicleStockInward/vehicleStockInward.worker.js');
 
-const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        logger.info(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    logger.info(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
+
