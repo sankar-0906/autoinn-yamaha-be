93190598ea1c8be1c1b246f2f0b8df90e4573e31
sankar <sankar.r@codingmart.com>
2026-04-28 12:06:26 +0530

    auth issue fixed

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -2,12 +2,12 @@ import Joi from 'joi';
 
 export const loginSchema = Joi.object({
     phone: Joi.string().required(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().required()
 });
 
 export const registerSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().required(),
     phone: Joi.string().required(),
     name: Joi.string().required()
 });
